9389f1bfefd2549e8762ee963736455bb1caec8c
RashAlGhul <zhenya.dima.98@mail.ru>
2017-08-03 04:58:36 -0700

    fields refactoring

--- smart_csharp/VK/PageObjects/MainPage.cs
+++ smart_csharp/VK/PageObjects/MainPage.cs
@@ -9,16 +9,16 @@ namespace WebdriverFramework.VK.PageObjects
     {
         #region Locators
         private readonly By _myPage = By.XPath(@"//span[contains(text(),'Моя Страница')]");
-        private By TextInspector(string text) => By.XPath($@"//div[contains(text(),'{text}')]");
-        private By LikeLocator(int postId) => By.XPath($@"//*[@id='post{Regex.Match(TestData.UserId, $@"{TestData.GetNumberRegex}").
-            Value}_{postId}']//span[@class='post_like_link _link']");
+        private By _textInspector = By.XPath($@"//div[contains(text(),'{_postText}')]");
+        private By _likeLocator = By.XPath($@"//*[@id='post{Regex.Match(TestData.UserId, $@"{TestData.GetNumberRegex}").
+            Value}_{_postId}']//span[@class='post_like_link _link']");
         private By ImageLocator(string imageId) => By.XPath($@"//a[@href='/{imageId}']");
 
         #endregion
 
-        private string _postText;
+        private static string _postText;
         private string _commentText;
-        private int _postId;
+        private static int _postId;
         private readonly HttpUtil _util = new HttpUtil();
         private BaseElement _post;
         private BaseElement _comment;
@@ -54,7 +54,7 @@ namespace WebdriverFramework.VK.PageObjects
 
         public void LikePost()
         {
-            BaseElement like = new Button(LikeLocator(_postId),"Like");
+            BaseElement like = new Button(_likeLocator,"Like");
             like.Click();
         }
 
@@ -67,25 +67,25 @@ namespace WebdriverFramework.VK.PageObjects
 
         public bool IsPostCreated()
         {
-            _post = new MenuItem(TextInspector(_postText), "Post");
+            _post = new MenuItem(_textInspector, "Post");
             return _post.IsPresent();
         }
 
         public bool IsPostDeleted()
         {
-            bool flag = new MenuItem(TextInspector(_postText), "Delete").IsExists();
+            bool flag = new MenuItem(_textInspector, "Delete").IsExists();
             return flag;
         }
 
         public bool IsPostEdited()
         {
-            _post = new MenuItem(TextInspector(_postText), "EditPost");
+            _post = new MenuItem(_textInspector, "EditPost");
             return _post.IsPresent() && new MenuItem(ImageLocator(_util.imageId), "Image").IsPresent();
         }
 
         public bool IsPostCommented()
         {
-            _comment = new MenuItem(TextInspector(_commentText), "Comment");
+            _comment = new MenuItem(_textInspector, "Comment");
             return _comment.IsPresent();
         }
     }
